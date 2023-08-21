3876826a15227c37208f77cf8a18b5146feeea59
Arty152 <zhirkov.atry@gmai.com>
2023-08-21 22:39:41 +0300

    Homework 4

--- src/phone.py
+++ src/phone.py
@@ -5,11 +5,12 @@ class Phone(Item):
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.__number_of_sim = number_of_sim
+        if number_of_sim <= 0 and isinstance(number_of_sim, int):
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self.number_of_sim = number_of_sim
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
-
 
     @property
     def number_of_sim(self):
@@ -17,6 +18,6 @@ class Phone(Item):
 
     @number_of_sim.setter
     def number_of_sim(self, value):
-        if value <= 0:
+        if value <= 0 and isinstance(value, int):
             raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
         self.__number_of_sim = value
